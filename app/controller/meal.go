3eda9ca6a8ea342aedf5718b6abda512adbf23ee
taijusugahara <96ayutuki@gmail.com>
2022-07-30 13:00:58 +0900

    mealの作成はmenu作成時またはmeal_image作成時に行う。既にmealが存在する場合は作成せずにそれを取得する--MealGetOrCreate

--- app/controller/meal.go
+++ app/controller/meal.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"go_meal_record/app/db"
 	"go_meal_record/app/model"
 	"go_meal_record/app/utils/common"
@@ -83,88 +82,16 @@ func MealIndexByMonth(c *gin.Context) {
 	})
 }
 
-// func MenuIndex(c *gin.Context) {
-// 	menus := []model.Menu{}
-// 	result := db.DB.Order("id").Find(&menus)
-// 	if result.Error != nil {
-// 		log.Println(result.Error)
-// 		c.String(http.StatusInternalServerError, "Server Error")
-// 	}
-// 	c.JSONP(http.StatusOK, gin.H{
-// 		"data": menus,
-// 	})
-// }
-
-func MealCreate(c *gin.Context) {
+func MealGetOrCreate(c *gin.Context, meal_type string, date_str string) (model.Meal, error) {
 	validate := validate.Validate()
 	var err error
 	meal := model.Meal{}
-	err = c.BindJSON(&meal)
-	fmt.Println(err)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "Server Error")
-		return
-	}
-	user, err := GetCurrentUser(c)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusBadRequest, "Not correct user")
-		return
-	}
-	meal.User = user
-	meal.UserID = user.ID
-	err = validate.Struct(meal)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "Validation Error")
-		return
-	}
-	date := meal.Date
-	previous_meal := model.Meal{}
-	//dateはmodelでdatatypes.Date使用しているので例えば2022:07:20T10:40:30Zは2022:07:20T:00:00:00Zとなる
-	is_already_create := db.DB.Where("meal_type = ? AND user_id = ? AND date = ?", meal.MealType, meal.UserID, date).First(&previous_meal)
-	//既に作成している場合は作成させない。
-	if is_already_create.Error == nil {
-		c.String(http.StatusInternalServerError, "The record already exist so cannot create.")
-		return
-	}
-	//
-	// now := time.Now()
-	// today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
-	// tomorrow := today.AddDate(0, 0, 1)
-	//
-	result := db.DB.Create(&meal)
-	// meal.CreatedAt = today
-	// db.DB.Save(&meal)
-	if result.Error != nil {
-		log.Println(err)
-		c.String(http.StatusInternalServerError, "Server Error")
-		return
-	}
-	log.Println(meal.ID)
-
-	c.JSONP(http.StatusOK, gin.H{
-		"data": meal,
-	})
-}
-
-func MenuCreate(c *gin.Context) {
-	validate := validate.Validate()
-	var err error
-	meal := model.Meal{}
-	menu := model.Menu{}
-	//js FormDataから渡してる。JsonBindしないのは渡ってきた3つが1つのstructに入るわけではないから。
-	meal_type := c.PostForm("meal_type")
-	date_str := c.PostForm("date")
-	menu_name := c.PostForm("menu")
-
 	already_created_meal := model.Meal{}
 	user, err := GetCurrentUser(c)
 	if err != nil {
 		log.Println(err)
 		c.String(http.StatusBadRequest, "Not correct user")
-		return
+		return meal, err
 	}
 	user_id := user.ID
 	date, _ := time.ParseInLocation("2006-01-02T15:04:05Z", date_str, time.Local)
@@ -183,21 +110,38 @@ func MenuCreate(c *gin.Context) {
 		if err != nil {
 			log.Println(err)
 			c.String(http.StatusInternalServerError, "Validation Error")
-			return
+			return meal, err
 		}
 
 		result := db.DB.Create(&meal)
 		if result.Error != nil {
 			log.Println(result.Error)
 			c.String(http.StatusInternalServerError, "Server Error")
-			return
+			return meal, result.Error
 		}
+	}
+
+	return meal, nil
+}
+
+func MenuCreate(c *gin.Context) {
+	validate := validate.Validate()
+	var err error
+	menu := model.Menu{}
+	//js FormDataから渡してる。JsonBindしないのは渡ってきた3つが1つのstructに入るわけではないから。
+	meal_type := c.PostForm("meal_type")
+	date_str := c.PostForm("date")
+	menu_name := c.PostForm("menu")
+
+	meal, err := MealGetOrCreate(c, meal_type, date_str)
+	if err != nil {
+		return
 	}
 	//同じ名前とmeal_idを持つmenuは作成させない。
 	already_created_menu := model.Menu{}
-	is_already_menu_result := db.DB.Where("name = ? AND meal_id = ?", menu_name, meal.ID).First(&already_created_menu)
-	if is_already_menu_result.Error == nil { //存在したら
-		log.Println(is_already_menu_result.Error)
+	// ErrRecordNotFound エラーを避けたい場合は、db.Limit(1).Find(&user)のように、Find を 使用することができます。
+	db.DB.Where("name = ? AND meal_id = ?", menu_name, meal.ID).Limit(1).Find(&already_created_menu)
+	if already_created_menu.ID != 0 { //存在したら
 		c.String(http.StatusInternalServerError, "既に同じ名前とmeal_idを持つmenuが存在するため作成できません。")
 		return
 	}
@@ -260,7 +204,6 @@ func MenuDelete(c *gin.Context) {
 func MealImageCreate(c *gin.Context) {
 	var err error
 	validate := validate.Validate()
-	meal := model.Meal{}
 	image := model.MealImage{}
 	//fileがある場合はjsonBindじゃなくてc.PostForm使っていけた。
 	//理由としてはheaderのcontent-typeをmultipart-form;にしてるからだと思う。いやデータ格納するのにFormData使ってるからかな
@@ -273,78 +216,42 @@ func MealImageCreate(c *gin.Context) {
 		return
 	}
 	//Menuがあるか確認。なければ作成
-	already_created_meal := model.Meal{}
-	user, err := GetCurrentUser(c)
-	if err != nil {
-		log.Println(err)
-		c.String(http.StatusBadRequest, "Not correct user")
-		return
-	}
-	user_id := user.ID
-	date, _ := time.ParseInLocation("2006-01-02T15:04:05Z", date_str, time.Local)
-	date_start := datatypes.Date(date)
-	// ErrRecordNotFound エラーを避けたい場合は、db.Limit(1).Find(&user)のように、Find を 使用することができます。
-	db.DB.Where("meal_type = ? AND user_id = ? AND date = ?", meal_type, user_id, date_start).Joins("User").Limit(1).Find(&already_created_meal)
-	//既に作成している場合
-	if already_created_meal.ID != 0 {
-		meal = already_created_meal
-	} else { //新しく作成する
-		meal.MealType = meal_type
-		meal.Date = date_start
-		meal.User = user
-		meal.UserID = user_id
+	meal, err := MealGetOrCreate(c, meal_type, date_str)
 
-		err = validate.Struct(meal)
+	filename := file.Filename
+	filename_split_dot := strings.Split(filename, ".")
+	extention := filename_split_dot[len(filename_split_dot)-1]
+	valid_extentions := []string{"jpeg", "jpg", "JPEG", "png", "PNG"}
+	if common.Contains(valid_extentions, extention) {
+		image.File = filename
+		image.Meal = meal
+		image.MealID = meal.ID
+		err = validate.Struct(image)
 		if err != nil {
 			log.Println(err)
 			c.String(http.StatusInternalServerError, "Validation Error")
 			return
 		}
-
-		result := db.DB.Create(&meal)
+		result := db.DB.Create(&image)
 		if result.Error != nil {
 			log.Println(result.Error)
 			c.String(http.StatusInternalServerError, "Server Error")
 			return
 		}
-	}
-
-	if file != nil {
-		filename := file.Filename
-		filename_split_dot := strings.Split(filename, ".")
-		extention := filename_split_dot[len(filename_split_dot)-1]
-		valid_extentions := []string{"jpeg", "jpg", "JPEG", "png", "PNG"}
-		if common.Contains(valid_extentions, extention) {
-			image.File = filename
-			image.Meal = meal
-			image.MealID = meal.ID
-			err = validate.Struct(image)
-			if err != nil {
-				log.Println(err)
-				c.String(http.StatusInternalServerError, "Validation Error")
-				return
-			}
-			result := db.DB.Create(&image)
-			if result.Error != nil {
-				log.Println(result.Error)
-				c.String(http.StatusInternalServerError, "Server Error")
-				return
-			}
-			id := strconv.Itoa(image.ID)
-			err := os.Mkdir("app/static/meal/"+id, 0750)
-			if err != nil {
-				log.Println(err)
-				c.String(http.StatusInternalServerError, "Server Error")
-				return
-			}
-			path := "app/" + image.File
-			c.SaveUploadedFile(file, path)
-		} else {
-			c.String(http.StatusInternalServerError, "File extention not correct")
+		id := strconv.Itoa(image.ID)
+		err := os.Mkdir("app/static/meal/"+id, 0750)
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusInternalServerError, "Server Error")
 			return
 		}
-
+		path := "app/" + image.File
+		c.SaveUploadedFile(file, path)
+	} else {
+		c.String(http.StatusInternalServerError, "File extention not correct")
+		return
 	}
+
 	c.JSONP(http.StatusOK, gin.H{
 		"image": image,
 	})
--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"github.com/gin-contrib/cors"
-	// limits "github.com/gin-contrib/size"
 	"github.com/gin-gonic/gin"
 
 	"go_meal_record/app/controller"
 	_ "go_meal_record/app/db" //module名/ディレクトリ initだけの場合は_使用
 	"go_meal_record/app/middleware"
 
-	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile) //行情報出るように。
 	engine := gin.Default()
-	// engine.MaxMultipartMemory = 1000 << 20
 	cors_config := cors.DefaultConfig()
 	cors_config.AllowOrigins = []string{"http://localhost:3001"}
 	cors_config.AllowHeaders = []string{"Access-Control-Allow-Credentials",
@@ -27,20 +26,16 @@ func main() {
 		"Authorization"}
 
 	engine.Use(cors.New(cors_config))
-	// engine.Use(limits.RequestSizeLimiter(100000000))
 	//ログイン必要なし
 	v1 := engine.Group("/v1")
 	{
 		account := v1.Group("/account")
-		fmt.Println(account)
-
 		{
-			// account.GET("/list", controller.BookList)
 			account.POST("/register", controller.AccountRegister)
 			account.POST("/login", controller.Login)
 		}
 	}
-	// 	//ログイン必要
+	//ログイン必要
 	v2 := engine.Group("/v2")
 	v2.Use(middleware.JwtAuthMiddleware())
 	{
@@ -56,8 +51,6 @@ func main() {
 		meal.GET("/index/day/:date/", controller.MealIndexByDay)
 		meal.GET("/index/week/:date/", controller.MealIndexByWeek)
 		meal.GET("/index/month/:date/", controller.MealIndexByMonth)
-		meal.POST("/create/", controller.MealCreate)
-		// meal.GET("/menu_index/", controller.MenuIndex)
 		meal.POST("/menu_create/", controller.MenuCreate)
 		//!!!!!!!下、最後に/(スラッシュ)つけてはいけない。/つけた場合は1MB以上のファイルを送信できなかった(POSTMAN)。Reactは試してない。https://stackoverflow.com/questions/33771167/handle-file-uploading-with-go
 		meal.POST("/image_create", controller.MealImageCreate)
